839bbdd084ec315b01180de55efd0d61be460f25
Shaun <Shaunwoodier@live.co.uk>
2021-10-14 14:09:42 +0100

    Add FighterService Interface

--- src/main/java/com/qa/service/FighterService.java
+++ src/main/java/com/qa/service/FighterService.java
@@ -1,5 +1,21 @@
 package com.qa.service;
 
+import java.util.List;
+
+import com.qa.data.Fighter;
+
 public interface FighterService {
+	
+	public Fighter getFigtherByIndex(Integer id);
+	
+	public List<Fighter> getAllFighters();
+	
+	public Fighter createFighter(Fighter fighter);
+	
+	public Fighter updateFighter(Fighter fighter, Integer id);
+	
+	public void deleteFighter(Integer id);
+	
+	
 
 }
