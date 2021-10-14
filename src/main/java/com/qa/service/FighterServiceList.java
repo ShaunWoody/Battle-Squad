f1bcf19b8a005946e758f40a93e8d91198f8c40c
Shaun <Shaunwoodier@live.co.uk>
2021-10-14 14:10:17 +0100

    Added FighterServiceList

--- src/main/java/com/qa/service/FighterServiceList.java
+++ src/main/java/com/qa/service/FighterServiceList.java
@@ -1,5 +1,47 @@
 package com.qa.service;
 
-public class FighterServiceList {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+
+import com.qa.data.Fighter;
+
+@Primary
+public class FighterServiceList implements FighterService {
+
+	private List<Fighter> fighters = new ArrayList<>();
+	
+	@Override
+	public Fighter getFigtherByIndex(Integer id) {
+		
+		return this.fighters.get(id);
+	}
+
+	@Override
+	public List<Fighter> getAllFighters() {
+		
+		return this.fighters;
+	}
+
+	@Override
+	public Fighter createFighter(Fighter fighter) {
+		this.fighters.add(fighter);
+		return this.fighters.get(this.fighters.size()-1);
+	}
+
+	@Override
+	public Fighter updateFighter(Fighter fighter, Integer id) {
+		return this.fighters.set(id,fighter);
+	}
+
+	@Override
+	public void deleteFighter(Integer id) {
+		Fighter delete = this.fighters.get(id);
+		this.fighters.remove(delete);
+		
+	}
+	
+	
 
 }
